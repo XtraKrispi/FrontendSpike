4795d2b14505860bfa91fefc8439b03995852110
unknown <MGold@BAR-DEV-GOLDIE8.toolbox.tbs>
2013-09-30 12:10:56 -0400

    Adding more functionality to the knockout code

--- FrontendSpike/Scripts/knockout/app.js
+++ FrontendSpike/Scripts/knockout/app.js
@@ -13,15 +13,19 @@
     var UserListViewModel = function () {
         this.users = ko.observableArray([]);
         var that = this;
-        $.get('/api/users').success(function (users) {
-            for (var i = 0; i < users.length; i++) {
-                var userFromService = users[i];
-                var user = new User(userFromService.UserId, userFromService.FirstName, userFromService.LastName, userFromService.Username, userFromService.EmailAddress, userFromService.CreatedDate, userFromService.ModifiedDate);
-                that.users.push(user);
-            }
-        }).error(function(err) {
 
-        });
+        this.getUsers = function () {
+            this.users([]);
+            $.get('/api/users').success(function (users) {
+                for (var i = 0; i < users.length; i++) {
+                    var userFromService = users[i];
+                    var user = new User(userFromService.UserId, userFromService.FirstName, userFromService.LastName, userFromService.Username, userFromService.EmailAddress, userFromService.CreatedDate, userFromService.ModifiedDate);
+                    that.users.push(user);
+                }
+            }).error(function (err) {
+
+            });
+        };
 
         this.removeUser = function (user) {
             $.ajax('/api/users/' + user.userId, {
@@ -34,12 +38,57 @@
         };
 
         this.selectUser = function(user) {
-
+            location.hash = 'users/' + user.userId;
         };
     };
 
-    var AddEditViewModel = function() {
-        this.user = ko.observable();
+    var AddEditViewModel = function () {
+        var that = this;
+        this.user = ko.observable(new User());
+        this.isUsernameTaken = ko.observable(false);
+        this.saveUser = function () {
+            var applyUser = function(user) {
+                that.clearUser();
+            };
+            
+            if (that.user()) {
+                var user = that.user();
+                if (that.user().userId) {
+                    $.ajax('/api/users/' + user.userId, { type: 'PUT', data: ko.toJSON(that.user) }).success(function() {
+                        applyUser(user);
+                    });
+                } else {
+                    $.post('/api/users', ko.toJSON(that.user)).success(function(data) {
+                        applyUser(data);
+                    });
+                }
+            }
+        };
+
+        this.checkUsername = function (viewModel) {
+            var username = viewModel.user().username();
+            $.get('/api/users?username=' + username).success(function(data) {
+                that.isUsernameTaken(data && data.length > 0 && username);
+            }).error(function(err) {
+
+            });
+        };
+
+        this.clearUser = function () {
+            this.user(new User());
+            location.hash = 'users';
+        };
+
+        this.getUser = function (id) {
+            var that = this;
+            //var user = new User()
+            $.get('/api/users/' + id).success(function(data) {
+                var user = new User(data.UserId, data.FirstName, data.LastName, data.Username, data.EmailAddress, data.createdDate, data.modifiedDate);
+                that.user(user);
+            }).error(function(err) {
+
+            });
+        };
     };
 
     var User = function (userId, firstName, lastName, username, emailAddress, createdDate, modifiedDate) {
@@ -61,4 +110,16 @@
 
     var addEditViewModel = new AddEditViewModel();
     ko.applyBindings(addEditViewModel, $('#edit')[0]);
+
+    Sammy(function () {
+        this.get('#users', function() {
+            userListViewModel.getUsers();
+            addEditViewModel.clearUser();
+        });
+
+        this.get('#users/:id', function() {
+            userListViewModel.getUsers();
+            addEditViewModel.getUser(this.params.id);
+        });
+    }).run();
 })();
